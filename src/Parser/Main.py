9b01f6fffd7f6726a6250241ec7dd7d9cde17cd9
Kv1sm8n <130540668+Kv1sm8n@users.noreply.github.com>
2024-12-20 22:57:12 +1100

    t1

--- src/Parser/Main.py
+++ src/Parser/Main.py
@@ -1,12 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://cultsakhalin.ru/"
+#url = "https://cultsakhalin.ru/"
 
-response = requests.get(url)
+#response = requests.get(url)
 
-bs = BeautifulSoup(response.text)
+#bs = BeautifulSoup(response.text)
 
-sect = bs.find("section", "IndexPage_section__2ATJp")
+html = '<html><head><title>Заголовок страницы</title></head><body> текст </body></html>'
 
-print(sect)
+#soup_html = BeautifulSoup('<html><head><title>Заголовок страницы</title></head><body></body></html>', 'html.parser')
+
+#sect = bs.find("section", "IndexPage_section__2ATJp")
+
+
+#html_file = open('test.html', "w", encoding="utf-8")
+
+#html_file.writelines(soup_html.prettify())
+
+def change_html_tag(html: str, container: str, tag: str = "<body>") -> str: 
+    alt_tag = tag.replace('<', '</')
+    html.replace(html[html.index(tag) + len(tag):html.index(alt_tag)], html[html.index(tag) + len(tag):html.index(alt_tag)] + container)
+    return html
+
+print(change_html_tag(html, 'сосите хуй негры'))
